4022c6de9f990587f78746177d0bda9f70f5aead
aman2705 <79598556+aman2705@users.noreply.github.com>
2022-01-22 10:48:22 +0530

    Time: 2 ms (20.00%), Space: 38.5 MB (50.44%) - LeetHub

---
+++ 709-to-lower-case/709-to-lower-case.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String toLowerCase(String s) {
+        // s=s.toLowerCase();
+        // return s;
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            int ascii=ch;
+            if(ascii>=65 && ascii<=90)
+            {
+                ascii=ascii+32;
+            }
+            ch=(char) ascii;
+            sb.append(ch);
+        }
+        return sb.toString();
+    }
+}
